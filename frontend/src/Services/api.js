89cefa57676293eb8c144bd2e3286d363c129ae2
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-06-28 22:55:55 +0530

    route update

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://taskmanager-58nq.onrender.com",
+  baseURL: "https://taskmanager-58nq.onrender.com/api",
 });
 
 export default API;
