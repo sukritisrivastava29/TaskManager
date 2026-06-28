662254ec9549f57f916fc68836113b500e7ef68b
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-06-28 22:45:21 +0530

    Update API URL for production

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,10 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
+   const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch((err) => console.log(err));
