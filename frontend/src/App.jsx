738293b9c4709629812a58a15558a5836a3b5caf
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-06-28 22:50:25 +0530

    Fix import path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import API from "./services/api";
+import API from "./Services/api";
 
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
